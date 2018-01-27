055e0b7c85518fb272fcdd4497901f679aff71b1
Josh Brown <brownbear1002@gmail.com>
2018-01-27 19:01:30 -0400

    Completed Chassis

--- src/org/usfirst/frc/team1677/robot/subsystems/Chassis.java
+++ src/org/usfirst/frc/team1677/robot/subsystems/Chassis.java
@@ -1,14 +1,25 @@
 package org.usfirst.frc.team1677.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.VictorSP;
 
 public class Chassis {
 	private Victor Left;
 	private Victor Right;
 	
 	public Chassis() {
-		Left = new Victor(0);
+		Left = (new Victor(0));
 		Right = new Victor(1);
+		
+		setLeft(0.0);
+		setRight(0.0);
 	}
+
+	private void setLeft(double input) {
+		Left.set(input);
+	}
+	
+	private void setRight(double input) {
+		Right.set(input);
+	}
+	
 }
