35f23b8113fa26fc7c5a895abed85fa8f7bab05f
Josh Brown <brownbear1002@gmail.com>
2018-02-06 17:34:04 -0500

    Update
    PID System Intergrated

--- src/org/usfirst/frc/team1677/robot/utility/Pid_Controller.java
+++ src/org/usfirst/frc/team1677/robot/utility/Pid_Controller.java
@@ -100,6 +100,133 @@ public class Pid_Controller {
 		}
 	}
 	
+	public void setOutputLimits(double minimum, double maximum){
+		if(maximum < minimum) {
+			return;
+		}else {
+			setMaxOutput(maximum);
+			setMinOutput(minimum);
+		}
+	}
+	
+	public double getOutput(double actual, double setpoint){
+		double output;
+		double Poutput;
+		double Ioutput;
+		double Doutput;
+		double Foutput;
+
+		this.setpoint=setpoint;
+
+		// Ramp the setpoint used for calculations if user has opted to do so
+		if(setpointRange!=0){
+			setpoint = constrain(setpoint,actual-setpointRange,actual+setpointRange);
+		}
+
+		// Do the simple parts of the calculations
+		double error=setpoint-actual;
+
+		// Calculate F output. Notice, this depends only on the setpoint, and not the error. 
+		Foutput=F*setpoint;
+
+		// Calculate P term
+		Poutput=P*error;   
+
+		// If this is our first time running this, we don't actually _have_ a previous input or output. 
+		// For sensor, sanely assume it was exactly where it is now.
+		// For last output, we can assume it's the current time-independent outputs. 
+		if(firstRun){
+			lastActual=actual;
+			lastOutput=Poutput+Foutput;
+			firstRun=false;
+		}
+
+		// Calculate D Term
+		// Note, this is negative. This actually "slows" the system if it's doing
+		// the correct thing, and small values helps prevent output spikes and overshoot 
+		Doutput= -D*(actual-lastActual);
+		lastActual=actual;
+
+		// The Iterm is more complex. There's several things to factor in to make it easier to deal with.
+		// 1. maxIoutput restricts the amount of output contributed by the Iterm.
+		// 2. prevent windup by not increasing errorSum if we're already running against our max Ioutput
+		// 3. prevent windup by not increasing errorSum if output is output=maxOutput    
+		Ioutput=I*errorSum;
+		if(maxIOutput!=0){
+			Ioutput=constrain(Ioutput,-maxIOutput,maxIOutput); 
+		}    
+
+		// And, finally, we can just add the terms up
+		output=Foutput + Poutput + Ioutput + Doutput;
+
+		// Figure out what we're doing with the error.
+		if(minOutput!=maxOutput && !bounded(output, minOutput,maxOutput) ){
+			errorSum=error; 
+			// reset the error sum to a sane level
+			// Setting to current error ensures a smooth transition when the P term 
+			// decreases enough for the I term to start acting upon the controller
+			// From that point the I term will build up as would be expected
+		}
+		else if(outputRampRate!=0 && !bounded(output, lastOutput-outputRampRate,lastOutput+outputRampRate) ){
+			errorSum=error; 
+		}
+		else if(maxIOutput!=0){
+			errorSum=constrain(errorSum+error,-maxError,maxError);
+			// In addition to output limiting directly, we also want to prevent I term 
+			// buildup, so restrict the error directly
+		}
+		else{
+			errorSum+=error;
+		}
+
+		// Restrict output to our specified output and ramp limits
+		if(outputRampRate!=0){
+			output=constrain(output, lastOutput-outputRampRate,lastOutput+outputRampRate);
+		}
+		if(minOutput!=maxOutput){ 
+			output=constrain(output, minOutput,maxOutput);
+			}
+		if(outputFilter!=0){
+			output=lastOutput*outputFilter+output*(1-outputFilter);
+		}
+
+		// Get a test printline with lots of details about the internal 
+		// calculations. This can be useful for debugging. 
+		// System.out.printf("Final output %5.2f [ %5.2f, %5.2f , %5.2f  ], eSum %.2f\n",output,Poutput, Ioutput, Doutput,errorSum );
+		// System.out.printf("%5.2f\t%5.2f\t%5.2f\t%5.2f\n",output,Poutput, Ioutput, Doutput );
+
+		lastOutput=output;
+		return output;
+	}
+
+	/**
+	 * Forces a value into a specific range
+	 * @param value input value
+	 * @param min maximum returned value
+	 * @param max minimum value in range
+	 * @return Value if it's within provided range, min or max otherwise 
+	 */
+	private double constrain(double value, double min, double max){
+		if(value > max){ return max;}
+		if(value < min){ return min;}
+		return value;
+	}  
+
+	/**
+	 * Test if the value is within the min and max, inclusive
+	 * @param value to test
+	 * @param min Minimum value of range
+	 * @param max Maximum value of range
+	 * @return true if value is within range, false otherwise
+	 */
+	private boolean bounded(double value, double min, double max){
+		// Note, this is an inclusive range. This is so tests like
+		// `bounded(constrain(0,0,1),0,1)` will return false.
+		// This is more helpful for determining edge-case behaviour
+		// than <= is.
+		return (min<value) && (value<max);
+	}
+	
 	public double getP() {
 		return P;
 	}
